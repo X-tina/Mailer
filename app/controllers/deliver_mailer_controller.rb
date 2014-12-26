90bf019a190e15c0f6c59ff179a539c2a6302481
Kristine <kristine@idapGit.(none)>
2014-12-26 11:27:32 +0200

    Send mails to list of mails

--- app/controllers/deliver_mailer_controller.rb
+++ app/controllers/deliver_mailer_controller.rb
@@ -6,8 +6,8 @@ class DeliverMailerController < ApplicationController
     @head = convert_to_text(:head) if head?
     @body = convert_to_text(:body) if body?
     @attach = convert_string_to_path(params[:attach_path]) if attach_path?
-    MailObject.create_mails(@head, @body, @attach).deliver
-    flash.now[:notice] = "Email was send"
+    MailObject.create_mails(@mails, @head, @body, @attach).deliver
+    flash.now[:notice] = "Email was send to: #{@mails}"
   end
 
 end
--- app/mailers/mail_object.rb
+++ app/mailers/mail_object.rb
@@ -1,18 +1,20 @@
 class MailObject < ActionMailer::Base
   include InputRequest
 
-  # default to: Proc.new { mail_request },
-          # from: 'Mailer@example.com'
-  default from: "username.idap@gmail.com",
-          to: "ex@gmail.com"
+  default from: "username.idap@gmail.com"
 
-  def create_mails(head, body, attachs)
-  	@head, @body, @attachments = head, body, attachs
+  def create_mails(mails, head, body, attachs)
+    @mails, @head, @body, @attachments = mails, head, body, attachs
 
     attach_name = @attachments.split("/").last
     attachments["#{attach_name}"] = File.read("#{@attachments}")
 
-    mail subject: @head
+    mail(to: @mails, subject: @head)
+  end
+
+  def mail_list(mails)
+    @mails = mails
+    @@mails = Proc.new { @mails }
   end
 
 end
